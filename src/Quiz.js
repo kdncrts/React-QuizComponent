f4277f8848a175cccfe74cd6750f087ef626feeb
kdncrts <kdncrts@gmail.com>
2020-03-10 11:31:29 -0600

    m4 import fix

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import QuizQuestion from './QuizQuestion';
-import QuizeEnd from './QuizEnd';
 import QuizEnd from './QuizEnd';
 
 let quizData = require('./quiz_data.json');
